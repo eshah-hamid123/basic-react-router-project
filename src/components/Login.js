e25a83ba52e9987c7ed5965e42724da151559e07
eshah-hamid123 <bcsf20m519@pucit.edu.pk>
2023-06-08 20:25:50 +0500

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,41 @@
 import { useNavigate } from "react-router-dom";
-
+import "./Login.css";
 function Login() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
-    const handleLogin = () => {
-        
-        navigate('home');
-    }
-    return (
+  const handleLogin = () => {
+    navigate("home");
+  };
+  return (
+    <div className="main">
+      <div className="sub-main">
         <div>
+          <div className="container-title">
             <h1>The 11'z</h1>
-            <label> Username</label>
-            <br />  
-            <input type="text" placeholder="Enter Username" />
-            <br />
-            <label> Password</label>
-            <br />
-            <input type="password" placeholder="Enter Password" />
-            <button onClick={handleLogin}>Login</button>
+          </div>
         </div>
-    )
+        <br />
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="Enter Username"
+            className="username"
+          />
+        </div>
+
+        <div className="input-box2">
+          <input
+            type="text"
+            placeholder="Enter Password"
+            className="username"
+          />
+        </div>
+        <div>
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
